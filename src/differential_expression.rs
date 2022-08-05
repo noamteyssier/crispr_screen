7e876360b0e30052051f221034ddb0833c00ddc9
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-04 22:23:56 -0700

    write norm to file to check

--- src/differential_expression.rs
+++ src/differential_expression.rs
@@ -1,5 +1,9 @@
+use std::fs::File;
+
 use anyhow::Result;
-use polars::prelude::DataFrame;
+use polars::{
+    prelude::{DataFrame, NamedFrom, CsvWriter, SerWriter}, 
+    series::Series};
 use crate::{
     utils::{parse_to_string_vec, parse_to_ndarray}, 
     math::{median_ratio_normalization, total_normalization}
@@ -24,10 +28,25 @@ pub fn mageck(
     let _gene_names = parse_to_string_vec(frame, columns[1])?;
 
     // Normalize
-    let _normed_matrix = match normalization {
+    let normed_matrix = match normalization {
         Normalization::MedianRatio => median_ratio_normalization(&count_matrix),
         Normalization::Total => total_normalization(&count_matrix)
     };
+
+    let mut normed_frame: DataFrame = normed_matrix
+        .columns()
+        .into_iter()
+        .enumerate()
+        .map(|(idx, x)| Series::new(
+                &labels[idx],
+                x.to_vec()))
+        .collect();
+
+    let mut file = File::create("norm.tab")?;
+    CsvWriter::new(&mut file)
+        .has_header(true)
+        .with_delimiter(b'\t')
+        .finish(&mut normed_frame)?;
 
     // Mean-Variance Modeling
 
