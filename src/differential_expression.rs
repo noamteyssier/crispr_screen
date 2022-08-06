d50827477aa73ec011659cd4788229193e95ed9a
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-05 17:04:31 -0700

    first pass on enrichment testing with negative binomial

--- src/differential_expression.rs
+++ src/differential_expression.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
-use std::fs::File;
-use ndarray::{s, Array2, Axis, Array1};
+use ndarray_rand::rand_distr::num_traits::Pow;
+use statrs::distribution::{NegativeBinomial, DiscreteCDF};
+use std::{fs::File, ops::{Div, Sub}};
+use ndarray::{s, Array2, Axis, Array1, Zip};
 use polars::prelude::{DataFrame, Series, NamedFrom, df, CsvWriter, SerWriter};
 use crate::{
     utils::{parse_to_string_vec, parse_to_ndarray}, 
@@ -42,6 +44,47 @@ fn model_mean_variance(
     adj_var
 }
 
+fn enrichment_test(
+    t_mean: f64,
+    r: f64,
+    p: f64,
+    is_positive: bool) -> f64
+{
+    let distribution = NegativeBinomial::new(r, p)
+        .expect("Unexpected parameterization of negative binomial");
+    let pvalue = distribution.cdf(t_mean.round() as u64);
+    if is_positive{
+        1. - pvalue
+    } else {
+        pvalue
+    }
+}
+
+fn enrichment_testing(
+    normed_matrix: &Array2<f64>,
+    adj_var: &Array1<f64>,
+    n_controls: usize) -> Array1<f64>
+{
+    let control_means = normed_matrix
+        .slice(s![.., ..n_controls])
+        .mean_axis(Axis(1))
+        .expect("Unexpected Empty Control Matrix")
+        .map(|x| if *x == 0. {1.} else {*x});
+    let treatment_means = normed_matrix
+        .slice(s![.., n_controls..])
+        .mean_axis(Axis(1))
+        .expect("Unexpected Empty Treatment Matrix");
+    
+    let param_r = (&control_means.mapv(|x| x.pow(2))).div(adj_var.sub(&control_means)).mapv(|x| if x > 0. { x } else { 1. });
+    let param_p = (&control_means).div(adj_var);
+    let positive_lfc: Array1<bool> = (&control_means).iter().zip(treatment_means.iter()).map(|(c, t)| c < t).collect();
+
+    Zip::from(&treatment_means)
+        .and(&param_r)
+        .and(&param_p)
+        .and(&positive_lfc)
+        .map_collect(|t_mean, r, p, is_positive| enrichment_test(*t_mean, *r, *p, *is_positive))
+}
 
 pub fn mageck(
     frame: &DataFrame,
@@ -62,11 +105,16 @@ pub fn mageck(
     // Mean-Variance Modeling
     let adj_var = model_mean_variance(&normed_matrix, labels_controls.len());
 
+    // sgRNA Ranking (Enrichment)
+    let pvalues = enrichment_testing(&normed_matrix, &adj_var, labels_controls.len());
 
     let mut frame = df!(
+        "sgrna" => _sgrna_names,
+        "gene" => _gene_names,
         "control" => normed_matrix.slice(s![.., 0]).to_vec(),
         "treatment" => normed_matrix.slice(s![.., 1]).to_vec(),
-        "adj_var" => adj_var.to_vec()
+        "adj_var" => adj_var.to_vec(),
+        "pvalues" => pvalues.to_vec()
     )?;
 
     let file = File::create("adj_var.tab")?;
@@ -74,7 +122,6 @@ pub fn mageck(
         .has_header(true)
         .with_delimiter(b'\t')
         .finish(&mut frame)?;
-    // sgRNA Ranking (Enrichment)
     
     // Gene Ranking (Aggregation)
     Ok(())
