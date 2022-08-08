44d0d2ded80509f4fbda4145416aa156f72b5bcd
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-08 15:14:20 -0700

    added argsort and normed rank calculation

--- src/math/rra.rs
+++ src/math/rra.rs
@@ -1,7 +1,7 @@
 use hashbrown::HashMap;
 use ndarray::Array1;
 
-pub fn encode_index(genes: &Vec<String>) -> Vec<usize>
+fn encode_index(genes: &Vec<String>) -> Vec<usize>
 {
     let mut total = 0usize;
     let mut map = HashMap::with_capacity(genes.len());
@@ -21,16 +21,40 @@ pub fn encode_index(genes: &Vec<String>) -> Vec<usize>
     encoding
 }
 
+fn argsort(array: &Array1<f64>) -> Vec<usize>
+{
+    let mut order = Array1::range(0., array.len() as f64, 1.)
+        .iter()
+        .map(|x| *x as usize)
+        .collect::<Vec<usize>>();
+    order.sort_by(|a, b| 
+        array[*a].partial_cmp(&array[*b]).unwrap() 
+    );
+    order
+}
+
+fn normed_ranks(array: &Array1<f64>) -> Array1<f64>
+{
+    argsort(array)
+        .iter()
+        .map(|x| (x + 1) as f64)
+        .map(|x| x / array.len() as f64)
+        .collect()
+}
+
+
 pub fn alpha_rra(
     pvalues: &Array1<f64>,
     genes: &Vec<String>) 
 {
     let encode = encode_index(genes);
+
 }
 
 #[cfg(test)]
 mod testing {
-    use super::encode_index;
+    use super::{encode_index, argsort, normed_ranks};
+    use ndarray::array;
 
     #[test]
     fn test_encoding() {
@@ -40,5 +64,20 @@ mod testing {
             .collect();
         let encoding = encode_index(&names);
         assert_eq!(encoding, vec![0, 1, 0, 2]);
+    }
+
+    #[test]
+    fn test_argsort() {
+        let floats = array![0.3, 0.2, 0.1, 0.4];
+        let order = argsort(&floats);
+        assert_eq!(order, vec![2, 1, 0, 3]);
+    }
+
+    #[test]
+    fn test_normed_ranks() {
+        let floats = array![0.3, 0.2, 0.1, 0.4];
+        let truth = (array![2., 1., 0., 3.] + 1.) / 4.;
+        let nranks = normed_ranks(&floats);
+        assert_eq!(nranks, truth);
     }
 }
