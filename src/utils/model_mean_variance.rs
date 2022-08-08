5f37b030c1d6b3ad7306a010292652ddb0bcd4f7
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-08 14:15:59 -0700

    added a line break for clarity

--- src/utils/model_mean_variance.rs
+++ src/utils/model_mean_variance.rs
@@ -14,6 +14,7 @@ pub fn model_mean_variance(
     let model_mean = model_matrix.mean_axis(Axis(1))
         .expect("Unexpected Empty Model Matrix");
     let model_var = model_matrix.var_axis(Axis(1), 1.);
+
     let control_mean = normed_matrix
         .slice(s![.., ..n_controls])
         .mean_axis(Axis(1))
