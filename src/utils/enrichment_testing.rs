fcabd8b47685923aeb4f92e5752617e6a2248f06
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-08 14:16:39 -0700

    replaced is_positive bool with better variable name, separated r and p calculation to separate functions, and separated survival function designation to separate function for clarity

--- src/utils/enrichment_testing.rs
+++ src/utils/enrichment_testing.rs
@@ -1,7 +1,5 @@
 use statrs::function::beta;
 use ndarray::{s, Array2, Axis, Array1, Zip};
-use ndarray_rand::rand_distr::num_traits::Pow;
-use std::ops::{Div, Sub};
 
 /// Calculates the negative binomial cumulative distribution if measuring depletion otherwise
 /// calculates the negative binomial survival function.
@@ -9,9 +7,9 @@ fn enrichment_test(
     t_mean: f64,
     r: f64,
     p: f64,
-    is_positive: bool) -> f64
+    use_survival: bool) -> f64
 {
-    if is_positive {
+    if use_survival {
         // implements survival function of the negative binomial distribution
         beta::beta_reg(t_mean.round() + 1.0, r, 1.0 - p)
     } else {
@@ -19,6 +17,41 @@ fn enrichment_test(
         // introducing any precision loss (may replace with cdf once updated in statrs)
         beta::beta_reg(r, t_mean.round() + 1.0, p)
     }
+}
+
+/// Calculates the negative binomial rate from the provided mean and variance.
+/// Calculated using the formula: 
+/// r = mean**2 / (var - mean)
+/// All r that is equal to 0.0 is set to 1.0
+fn calculate_r(
+    mean: &Array1<f64>, 
+    var: &Array1<f64>) -> Array1<f64>
+{
+    let r = (mean * mean) / (var - mean);
+    r.mapv(|x| if x > 0. {x} else { 1. })
+}
+
+/// Calculates the negative binomial probability from the provided mean and variance.
+/// Calculated using the formula:
+/// p = mean / var
+fn calculate_p(
+    mean: &Array1<f64>,
+    var: &Array1<f64>) -> Array1<f64>
+{
+    mean / var
+}
+
+/// A boolean array to designate whether the survival function or the cumulative density function
+/// should be used to calculate the pvalue. If the treatment mean is greater than the control then
+/// this will return a true value in that position indicating the survival function should be used.
+fn calculate_survival(
+    control_means: &Array1<f64>,
+    treatment_means: &Array1<f64>) -> Array1<bool>
+{
+    control_means.iter()
+        .zip(treatment_means.iter())
+        .map(|(c, t)| c < t)
+        .collect()
 }
 
 /// Performs enrichment testing using a negative binomial distribution
@@ -37,14 +70,14 @@ pub fn enrichment_testing(
         .mean_axis(Axis(1))
         .expect("Unexpected Empty Treatment Matrix");
     
-    let param_r = (&control_means.mapv(|x| x.pow(2))).div(adj_var.sub(&control_means)).mapv(|x| if x > 0. { x } else { 1. });
-    let param_p = (&control_means).div(adj_var);
-    let positive_lfc: Array1<bool> = (&control_means).iter().zip(treatment_means.iter()).map(|(c, t)| c < t).collect();
+    let param_r = calculate_r(&control_means, adj_var);
+    let param_p = calculate_p(&control_means, adj_var);
+    let use_survival: Array1<bool> = calculate_survival(&control_means, &treatment_means);
 
     Zip::from(&treatment_means)
         .and(&param_r)
         .and(&param_p)
-        .and(&positive_lfc)
-        .map_collect(|t_mean, r, p, is_positive| enrichment_test(*t_mean, *r, *p, *is_positive))
+        .and(&use_survival)
+        .map_collect(|t_mean, r, p, use_survival| enrichment_test(*t_mean, *r, *p, *use_survival))
 }
 
