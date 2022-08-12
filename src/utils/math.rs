5296922f758ef3a7fd12f6ae439d37af16b83101
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-11 18:53:51 -0700

    added a zscore filter on the logged ols calculation (#24)

---
+++ src/utils/math.rs
@@ -0,0 +1,26 @@
+use std::ops::{Sub, Div};
+use ndarray::Array1;
+
+/// Z-Score Transforms the Provided Array
+pub fn zscore_transform(array: &Array1<f64>) -> Array1<f64>
+{
+    let mu = array.mean().unwrap_or(0.);
+    let sigma = array.std(0.);
+    array.sub(mu).div(sigma)
+}
+
+#[cfg(test)]
+mod testing {
+    use ndarray::Array1;
+    use ndarray_rand::{RandomExt, rand_distr::Normal};
+    use super::zscore_transform;
+
+    #[test]
+    fn test_zscore() {
+        let x = Array1::random(100000, Normal::new(100., 300.).unwrap());
+        let z = zscore_transform(&x);
+        
+        assert!((&z).mean() < Some(1e-6));
+        assert!(z.std(0.) - 1. < 1e-6);
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,5 @@
 mod dfutils;
 pub mod io;
+pub mod math;
 pub use dfutils::{parse_to_string_vec, parse_to_ndarray};
 
