32d27222eb3216881750c7e8c48bfdad9904111d
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-12 17:42:57 -0700

    return both scores and empirical cdf

--- src/aggregation/rra/alpha_rra.rs
+++ src/aggregation/rra/alpha_rra.rs
@@ -14,12 +14,15 @@ fn gene_rra(
     encodings: &[usize],
     nranks: &Array1<f64>,
     permutation_vectors: &HashMap<usize, Array1<f64>>,
-    alpha: f64) -> f64
+    alpha: f64) -> (f64, f64)
 {
     let gene_ranks = select_ranks(current_idx, encodings, nranks);
     let filtered = filter_alpha(&gene_ranks, alpha);
     let score = robust_rank_aggregation(&filtered, gene_ranks.len());
-    empirical_cdf(score, permutation_vectors.get(&gene_ranks.len()).expect("Unexpected missing key"))
+    (
+        score,
+        empirical_cdf(score, permutation_vectors.get(&gene_ranks.len()).expect("Unexpected missing key"))
+    )
 }
 
 /// Performs the alpha-RRA algorithm
@@ -28,7 +31,7 @@ pub fn alpha_rra(
     genes: &Vec<String>,
     alpha: f64,
     npermutations: usize,
-    logger: &Logger) -> (Vec<String>, Array1<f64>)
+    logger: &Logger) -> (Vec<String>, Array1<f64>, Array1<f64>)
 {
     let (encode_map, encode) = encode_index(genes);
     let n_genes = encode_map.len();
@@ -42,15 +45,19 @@ pub fn alpha_rra(
         .map(|unique_size| (*unique_size, run_permutations(nranks.len(), alpha, npermutations * n_genes, *unique_size)))
         .map(|(u, v)| (u, Array1::from_vec(v)))
         .collect::<HashMap<usize, Array1<f64>>>();
-    
+
     // calculate empirical pvalues for each of the gene sets given the random nulls
-    let pvalues = (0..*encode.iter().max().expect("Unexpected empty encoding"))
+    let (scores, pvalues): (Vec<f64>, Vec<f64>) = (0..*encode.iter().max().expect("Unexpected empty encoding"))
         .map(|curr| gene_rra(curr, &encode, &nranks, &permutation_vectors, alpha))
-        .collect::<Array1<f64>>();
+        .unzip();
 
     let names = (0..*encode.iter().max().expect("Unexpected empty encoding"))
         .map(|curr| encode_map.get(&curr).expect("Unexpected missing index").clone())
         .collect();
 
-    (names, pvalues)
+    (
+        names, 
+        Array1::from_vec(scores),
+        Array1::from_vec(pvalues)
+    )
 }
