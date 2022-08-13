fdae6e95e9e69a77a70626d7d530737dbad986fa
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-12 21:17:57 -0700

    confirms the array is sorted on each call + added more robust test functions

--- src/aggregation/rra/robust_rank.rs
+++ src/aggregation/rra/robust_rank.rs
@@ -1,17 +1,22 @@
 use statrs::distribution::{Beta, ContinuousCDF};
 use ndarray::Array1;
 
+/// Sorts the rank array in ascending order
+pub fn sort_array(array: &Array1<f64>) -> Array1<f64>
+{
+    let mut vec = array.to_vec();
+    vec.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+    Array1::from_vec(vec)
+}
+
 /// performs robust rank aggregation
 pub fn robust_rank_aggregation(
     normed_ranks: &Array1<f64>,
     num_values: usize) -> f64
 {
     if normed_ranks.is_empty() { return 1. }
-    let mut tmp = normed_ranks.to_vec();
-    tmp.sort_floats();
 
-    //normed_ranks
-    tmp
+    sort_array(normed_ranks)
         .iter()
         .enumerate()
         .map(|(k, n)| (k+1, num_values - k, n))
@@ -22,9 +27,10 @@ pub fn robust_rank_aggregation(
 
 #[cfg(test)]
 mod testing {
-    use super::robust_rank_aggregation;
+    use super::{robust_rank_aggregation, sort_array};
     use ndarray_rand::rand::{thread_rng, Rng};
-    use ndarray::Array1;
+    use statrs::distribution::{Beta, ContinuousCDF};
+    use ndarray::{Array1, array};
 
     #[test]
     fn test_robust_rank(){
@@ -34,5 +40,40 @@ mod testing {
         let rra = robust_rank_aggregation(&arr, num);
         assert!(rra > 0.);
         assert!(rra <= 1.);
+    }
+
+    #[test]
+    fn test_robust_rank_empty() {
+        for num in 0..100 {
+            let arr = array![];
+            let rra = robust_rank_aggregation(&arr, num);
+            assert_eq!(rra, 1.);
+        }
+    }
+
+    #[test]
+    fn test_robust_rank_sorted() {
+        let num = 5;
+        let ranks_a = array![0.5, 0.3, 0.2];
+        let ranks_b = array![0.3, 0.2, 0.5];
+        let rra_a = robust_rank_aggregation(&ranks_a, num);
+        let rra_b = robust_rank_aggregation(&ranks_b, num);
+        assert_eq!(rra_a, rra_b);
+    }
+
+    #[test]
+    fn test_robust_rank_exact() {
+        let num = 5;
+        let ranks = array![1e-6];
+        let rra = robust_rank_aggregation(&ranks, num);
+        let exact = Beta::new(1., num as f64).unwrap().cdf(ranks[0]);
+        assert_eq!(rra, exact);
+    }
+
+    #[test]
+    fn test_sorting() {
+        let arr = array![3., 2., 1.,];
+        let sorted = sort_array(&arr);
+        assert_eq!(sorted, array![1., 2., 3.,]);
     }
 }
