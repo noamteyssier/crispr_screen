ea78256635b63b23f6b8da64d519c39121fdb90a
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-19 16:27:41 -0700

    moved dataframe creation to separate functions and call them from mageck

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,5 +1,10 @@
 use std::fs::File;
-use polars::prelude::{CsvReader, CsvWriter, SerReader, SerWriter, DataFrame, PolarsError};
+use polars::prelude::{CsvReader, CsvWriter, SerReader, SerWriter, DataFrame, PolarsError, df, Series, NamedFrom};
+use ndarray::{Array2, Array1, s};
+use crate::{
+    enrich::EnrichmentResult,
+    aggregation::AggregationResult
+};
 
 /// Reads a dataframe from file
 pub fn load_dataframe(path: &str) -> Result<DataFrame, PolarsError>
@@ -32,5 +37,40 @@ pub fn write_gene_results(prefix: &str, frame: &mut DataFrame) -> Result<(), Pol
 {
     let filename = format!("{}.gene_results.tab", prefix);
     write_dataframe(&filename, frame)
+}
+
+/// Builds the gene-level aggregation results dataframe
+pub fn build_gene_dataframe(aggregation_results: &AggregationResult) -> Result<DataFrame, PolarsError>
+{
+    df!(
+        "gene" => aggregation_results.genes(),
+        "score_low" => aggregation_results.score_low().to_vec(),
+        "pvalue_low" => aggregation_results.pvalues_low().to_vec(),
+        "fdr_low" => aggregation_results.fdr_low().to_vec(),
+        "score_high" => aggregation_results.score_high().to_vec(),
+        "pvalue_high" => aggregation_results.pvalues_high().to_vec(),
+        "fdr_high" => aggregation_results.fdr_high().to_vec(),
+    )
+}
+
+/// Builds the sgrna-level differential expression results dataframe
+pub fn build_sgrna_dataframe(
+    sgrna_names: &[String],
+    gene_names: &[String],
+    normed_matrix: &Array2<f64>,
+    adj_var: &Array1<f64>,
+    sgrna_results: &EnrichmentResult) -> Result<DataFrame, PolarsError>
+{
+    df!(
+        "sgrna" => &sgrna_names,
+        "gene" => gene_names,
+        "control" => normed_matrix.slice(s![.., 0]).to_vec(),
+        "treatment" => normed_matrix.slice(s![.., 1]).to_vec(),
+        "adj_var" => adj_var.to_vec(),
+        "pvalue_low" => sgrna_results.pvalues_low().to_vec(),
+        "pvalue_high" => sgrna_results.pvalues_high().to_vec(),
+        "pvalue_twosided" => sgrna_results.pvalues_twosided().to_vec(),
+        "fdr" => sgrna_results.fdr().to_vec()
+    )
 }
 
