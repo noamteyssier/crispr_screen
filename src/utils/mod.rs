51bb120d1137f0f38188a9775beba4ec9931344b
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-17 17:01:00 -0800

    25 better error handling is control labels or treatment labes are not found in dataset (#45)
    * included thiserror for custom error handling
    
    * call specific parse functions for cleaner error handling
    
    * wrap main in result for cleaner error handling
    
    * export more functions from dfutils
    
    * split parsing functions, added error handling, and incorporated testing

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,5 +2,5 @@ mod dfutils;
 pub mod io;
 pub mod math;
 pub mod logging;
-pub use dfutils::{parse_to_string_vec, parse_to_ndarray};
+pub use dfutils::{parse_to_string_vec, parse_to_ndarray, parse_genes, parse_sgrna, FormatError};
 
