b385b1c335bdcd244a40eeee7e8a456fd0aaff72
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-25 12:46:18 -0800

    include model choice config struct

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,3 +1,5 @@
+use clap::ValueEnum;
+
 mod model_mean_variance;
 mod logged_ols;
 mod ols;
@@ -7,3 +9,9 @@ pub use model_mean_variance::model_mean_variance;
 use ols::Ols;
 use wols::Wols;
 use logged_ols::LoggedOls;
+
+#[derive(ValueEnum, Debug, Clone)]
+pub enum ModelChoice {
+    Ols,
+    Wols
+}
