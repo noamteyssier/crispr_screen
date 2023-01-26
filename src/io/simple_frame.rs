2b094b45dde135ccfeec63cf90f3edc78578914d
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-26 15:26:53 -0800

    75 increase testing coverage (#76)
    * perform io generically for easier testing
    
    * validate input on datamatrix call
    
    * added testing for dataframe io
    
    * cargo formatting
    
    * added testing on compute aggregation
    
    * added testing on config

--- src/io/simple_frame.rs
+++ src/io/simple_frame.rs
@@ -17,10 +17,10 @@ pub struct SimpleFrame {
 }
 impl SimpleFrame {
     pub fn from_filepath(path: &str) -> Result<Self> {
-        let headers = Self::parse_headers(path)?;
+        let headers = Self::parse_headers(&mut Self::read_file(path)?)?;
         let mut meta = Self::build_meta_hashmap(&headers);
         let mut data = Self::build_data_hashmap(&headers);
-        Self::parse_to_hashmaps(path, &headers, &mut meta, &mut data)?;
+        Self::parse_to_hashmaps(&mut Self::read_file(path)?, &headers, &mut meta, &mut data)?;
         Ok(Self {
             headers,
             meta,
@@ -28,10 +28,26 @@ impl SimpleFrame {
         })
     }
 
-    fn parse_headers(filename: &str) -> Result<Vec<String>> {
-        let mut file_buffer = File::open(filename).map(BufReader::new)?;
+    #[allow(dead_code)]
+    pub fn from_string(string: &str) -> Result<Self> {
+        let headers = Self::parse_headers(&mut string.as_bytes())?;
+        let mut meta = Self::build_meta_hashmap(&headers);
+        let mut data = Self::build_data_hashmap(&headers);
+        Self::parse_to_hashmaps(&mut string.as_bytes(), &headers, &mut meta, &mut data)?;
+        Ok(Self {
+            headers,
+            meta,
+            data,
+        })
+    }
+
+    fn read_file(filename: &str) -> Result<BufReader<File>> {
+        Ok(File::open(filename).map(BufReader::new)?)
+    }
+
+    fn parse_headers<R: BufRead>(buffer: &mut R) -> Result<Vec<String>> {
         let mut header_row = String::new();
-        file_buffer.read_line(&mut header_row)?;
+        buffer.read_line(&mut header_row)?;
         let headers = header_row
             .trim()
             .split('\t')
@@ -53,17 +69,17 @@ impl SimpleFrame {
             map
         })
     }
-    fn parse_to_hashmaps(
-        filename: &str,
+
+    fn parse_to_hashmaps<R: BufRead>(
+        buffer: &mut R,
         headers: &[String],
         meta_hash: &mut HashMap<String, Vec<String>>,
         data_hash: &mut HashMap<String, Vec<f64>>,
     ) -> Result<()> {
-        let file_buffer = File::open(filename).map(BufReader::new)?;
         let mut reader = ReaderBuilder::new()
             .has_headers(true)
             .delimiter(b'\t')
-            .from_reader(file_buffer);
+            .from_reader(buffer);
 
         for result in reader.deserialize() {
             let record: Record = result?;
@@ -85,6 +101,20 @@ impl SimpleFrame {
                 }
             }
         }
+
+        Self::validate_input(headers, &meta_hash, &data_hash)
+    }
+
+    fn validate_input(
+        headers: &[String],
+        meta_hash: &HashMap<String, Vec<String>>,
+        data_hash: &HashMap<String, Vec<f64>>,
+    ) -> Result<()> {
+        // Checks if there were not enough headers read
+        if headers.len() != meta_hash.len() + data_hash.len() {
+            bail!("Malformed data entry in Axis(0)")
+        }
+
         Ok(())
     }
 
@@ -96,6 +126,10 @@ impl SimpleFrame {
         if !self.valid_headers(labels) {
             bail!("Invalid headers provided")
         }
+        if labels.len() != self.data.keys().len() {
+            bail!("Provided count matrix is of unexpected shape!\nMust be [sgrna, gene, samples ... ]")
+        }
+
         let matrix = labels
             .iter()
             .map(|x| {
@@ -124,5 +158,118 @@ impl SimpleFrame {
 
     pub fn get_gene_names(&self) -> &[String] {
         self.meta.get(&self.headers[1]).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod testing {
+    use super::SimpleFrame;
+    use ndarray_rand::rand::random;
+
+    fn example_dataset() -> String {
+        let mut s = String::with_capacity(1000);
+        let headers = ["sgrna", "gene", "low_1", "low_2", "high_1", "high_2"];
+        let num_rows = 10;
+
+        headers.iter().enumerate().for_each(|(idx, x)| {
+            if idx > 0 {
+                s.push_str(&format!("\t{x}"))
+            } else {
+                s.push_str(&format!("{x}"))
+            }
+        });
+        s.push_str("\n");
+
+        (0..num_rows).for_each(|row_id| {
+            headers.iter().enumerate().for_each(|(idx, _)| {
+                if idx == 0 {
+                    s.push_str(&format!("sgrna_{row_id}"));
+                } else if idx == 1 {
+                    s.push_str(&format!("\tgene_{}", row_id % 5));
+                } else {
+                    s.push_str(&format!("\t{}", random::<f64>()))
+                }
+            });
+            s.push_str("\n");
+        });
+        s
+    }
+
+    fn broken_dataset() -> String {
+        let mut s = String::with_capacity(1000);
+        let headers = ["sgrna", "low_1", "low_2", "high_1", "high_2"];
+        let num_rows = 10;
+
+        headers.iter().enumerate().for_each(|(idx, x)| {
+            if idx > 0 {
+                s.push_str(&format!("\t{x}"))
+            } else {
+                s.push_str(&format!("{x}"))
+            }
+        });
+        s.push_str("\n");
+
+        (0..num_rows).for_each(|row_id| {
+            headers.iter().enumerate().for_each(|(idx, _)| {
+                if idx == 0 {
+                    s.push_str(&format!("sgrna_{row_id}"));
+                } else {
+                    s.push_str(&format!("\t{}", random::<f64>()))
+                }
+            });
+            s.push_str("\n");
+        });
+        s
+    }
+
+    #[test]
+    fn test_simple_frame() {
+        let datastream = example_dataset();
+        let frame = SimpleFrame::from_string(&datastream).unwrap();
+
+        assert_eq!(
+            frame.headers,
+            vec!["sgrna", "gene", "low_1", "low_2", "high_1", "high_2"]
+        );
+
+        let dm = frame
+            .data_matrix(
+                &vec!["low_1", "low_2", "high_1", "high_2"]
+                    .into_iter()
+                    .map(|x| x.to_owned())
+                    .collect::<Vec<String>>(),
+            )
+            .unwrap();
+        assert_eq!(dm.shape(), &[10, 4]);
+    }
+
+    #[test]
+    fn test_missing_label() {
+        let datastream = example_dataset();
+        let frame = SimpleFrame::from_string(&datastream).unwrap();
+        let dm = frame.data_matrix(
+            &vec!["low_1", "low_2", "high_1", "missing_label"]
+                .into_iter()
+                .map(|x| x.to_owned())
+                .collect::<Vec<String>>(),
+        );
+        assert!(dm.is_err());
+    }
+
+    #[test]
+    fn test_broken_dataset() {
+        let datastream = broken_dataset();
+        let frame = SimpleFrame::from_string(&datastream).unwrap();
+        assert_eq!(
+            frame.headers,
+            vec!["sgrna", "low_1", "low_2", "high_1", "high_2"]
+        );
+        let dm = frame.data_matrix(
+            &vec!["low_1", "low_2", "high_1", "high_2"]
+                .into_iter()
+                .map(|x| x.to_owned())
+                .collect::<Vec<String>>(),
+        );
+        assert!(dm.is_err());
     }
 }
