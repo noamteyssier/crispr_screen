12944e4f33299da0f53df098cc1369cc64339d82
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-26 15:22:26 -0800

    added testing on config

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,6 +1,7 @@
 use crate::{aggregation::GeneAggregation, model::ModelChoice, norm::Normalization};
 use adjustp::Procedure;
 
+#[derive(Debug)]
 pub struct Configuration<'a> {
     normalization: Normalization,
     aggregation: GeneAggregation<'a>,
@@ -39,4 +40,65 @@ impl<'a> Configuration<'a> {
     pub fn prefix(&self) -> &str {
         self.prefix
     }
+}
+
+#[cfg(test)]
+mod testing {
+    use adjustp::Procedure;
+
+    use crate::{norm::Normalization, aggregation::GeneAggregation, model::ModelChoice};
+
+    use super::Configuration;
+
+    fn build_config<'a>() -> Configuration<'a> {
+        let normalization = Normalization::MedianRatio;
+        let aggregation = GeneAggregation::Inc { token: "non-targeting" };
+        let correction = Procedure::BenjaminiHochberg;
+        let model_choice = ModelChoice::Wols;
+        let prefix = "results";
+        Configuration::new(normalization, aggregation, correction, model_choice, prefix)
+    }
+
+    #[test]
+    fn test_normalization() {
+        let config = build_config();
+        match config.normalization() {
+            Normalization::MedianRatio => assert!(true),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_aggregation() {
+        let config = build_config();
+        match config.aggregation() {
+            GeneAggregation::Inc { token } => assert_eq!(token, &"non-targeting"),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_correction() {
+        let config = build_config();
+        match config.correction {
+            Procedure::BenjaminiHochberg => assert!(true),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_model_choice() {
+        let config = build_config();
+        match config.model_choice(){
+            ModelChoice::Wols => assert!(true),
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_prefix() {
+        let config = build_config();
+        assert_eq!(config.prefix(), "results");
+    }
+
 }
