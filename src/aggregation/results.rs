42a84f7221570983ca3f615b044fb57ead0a2b4e
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-27 15:36:33 -0800

    66 add gene score to gene results (#86)
    * added phenotype score to gene frame
    
    * ran cargo formatting
    
    * bump version
    
    * include fdr column to gene output which represents minimum fdr

--- src/aggregation/results.rs
+++ src/aggregation/results.rs
@@ -1,5 +1,5 @@
 use adjustp::{adjust, Procedure};
-use ndarray::Array1;
+use ndarray::{Array1, Zip};
 
 pub struct AggregationResult {
     genes: Vec<String>,
@@ -11,6 +11,8 @@ pub struct AggregationResult {
     aggregation_score_high: Array1<f64>,
     fdr_low: Array1<f64>,
     fdr_high: Array1<f64>,
+    fdr: Array1<f64>,
+    phenotype_score: Array1<f64>,
 }
 impl AggregationResult {
     pub fn new(
@@ -24,7 +26,9 @@ impl AggregationResult {
     ) -> Self {
         let fdr_low = Self::fdr_adjustment(&pvalues_low, correction);
         let fdr_high = Self::fdr_adjustment(&pvalues_high, correction);
+        let fdr = Self::select_fdr(&fdr_low, &fdr_high);
         let gene_log2_fc = Self::calculate_log_fold_change(&gene_fc);
+        let phenotype_score = Self::calculate_phenotype_score(&fdr, &gene_log2_fc);
         Self {
             genes,
             gene_fc,
@@ -35,6 +39,8 @@ impl AggregationResult {
             aggregation_score_high,
             fdr_low,
             fdr_high,
+            fdr,
+            phenotype_score,
         }
     }
 
@@ -42,8 +48,26 @@ impl AggregationResult {
         Array1::from_vec(adjust(pvalues.as_slice().unwrap(), correction))
     }
 
+    fn select_fdr(fdr_low: &Array1<f64>, fdr_high: &Array1<f64>) -> Array1<f64> {
+        Zip::from(fdr_low)
+            .and(fdr_high)
+            .map_collect(|fdr_low, fdr_high| fdr_low.min(*fdr_high))
+    }
+
     fn calculate_log_fold_change(gene_fc: &Array1<f64>) -> Array1<f64> {
         gene_fc.mapv(f64::log2)
+    }
+
+    fn calculate_phenotype_score(
+        fdr: &Array1<f64>,
+        gene_log2_fc: &Array1<f64>,
+    ) -> Array1<f64> {
+        Zip::from(fdr)
+            .and(gene_log2_fc)
+            .map_collect(|fdr, gene_log2_fc| {
+                let nlfdr = -(fdr.log10());
+                nlfdr * gene_log2_fc
+            })
     }
 
     pub fn genes(&self) -> &Vec<String> {
@@ -80,6 +104,14 @@ impl AggregationResult {
 
     pub fn fdr_high(&self) -> &Array1<f64> {
         &self.fdr_high
+    }
+
+    pub fn fdr(&self) -> &Array1<f64> {
+        &self.fdr
+    }
+
+    pub fn phenotype_score(&self) -> &Array1<f64> {
+        &self.phenotype_score
     }
 }
 
@@ -119,5 +151,10 @@ mod testing {
         assert_eq!(result.score_high(), &Array1::from(vec![0.7, 0.8]));
         assert_eq!(result.fdr_low(), &Array1::from(vec![0.2, 0.2]));
         assert_eq!(result.fdr_high(), &Array1::from(vec![0.4, 0.4]));
+        assert_eq!(result.fdr(), &Array1::from(vec![0.2, 0.2]));
+        assert_eq!(
+            result.phenotype_score(),
+            &Array1::from(vec![0.0, 0.6989700043360187])
+        );
     }
 }
