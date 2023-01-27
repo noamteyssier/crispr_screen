5c2a59c608f7ccdd95eacc96d516ce0211b39bf1
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-01-27 13:22:57 -0800

    split compute aggregation into subfunctions, and move all utility functions and testing to agg-utils

--- src/aggregation/compute_aggregation.rs
+++ src/aggregation/compute_aggregation.rs
@@ -1,11 +1,10 @@
-use super::{alpha_rra, inc, AggregationResult};
+use super::{alpha_rra, inc, AggregationResult, utils::{set_alpha_threshold, filter_zeros}};
 use crate::{
     enrich::EnrichmentResult,
     utils::{agg::aggregate_fold_changes, logging::Logger},
 };
 use adjustp::Procedure;
-use hashbrown::HashSet;
-use ndarray::{Array1, Array2, Axis};
+use ndarray::{Array1, Array2};
 
 /// Enum describing the different gene aggregation procedures and their associated configurations.
 #[derive(Debug)]
@@ -20,75 +19,89 @@ pub enum GeneAggregation<'a> {
     },
 }
 
-/// Return all indices where values are above zero
-fn mask_zeros(array: &Array1<f64>, logger: &Logger) -> HashSet<usize> {
-    let mask = array
-        .iter()
-        .enumerate()
-        .filter(|(_idx, x)| **x > 0.)
-        .map(|(idx, _)| idx)
-        .collect::<HashSet<usize>>();
-    logger.num_zeros(array.len() - mask.len());
-    mask
+/// Aggregates the results of the gene aggregation analysis for internal use
+struct InternalAggregationResult {
+    genes: Vec<String>,
+    gene_scores_low: Array1<f64>,
+    gene_pvalues_low: Array1<f64>,
+    gene_scores_high: Array1<f64>,
+    gene_pvalues_high: Array1<f64>,
 }
-
-/// Filter `sgRNAs` with zero counts in both samples
-fn filter_zeros(
-    normed_matrix: &Array2<f64>,
-    gene_names: &[String],
-    sgrna_pvalues_low: &Array1<f64>,
-    sgrna_pvalues_high: &Array1<f64>,
-    logger: &Logger,
-) -> (Vec<String>, Array1<f64>, Array1<f64>) {
-    let sgrna_means = normed_matrix.mean_axis(Axis(1)).unwrap();
-    let passing_indices = mask_zeros(&sgrna_means, logger);
-    let mut sorted_indices = passing_indices.iter().copied().collect::<Vec<usize>>();
-    sorted_indices.sort_unstable();
-
-    let passing_gene_names = sorted_indices
-        .iter()
-        .map(|x| gene_names[*x].clone())
-        .collect::<Vec<String>>();
-    let passing_sgrna_pvalues_low = sorted_indices
-        .iter()
-        .map(|x| sgrna_pvalues_low[*x])
-        .collect::<Array1<f64>>();
-    let passing_sgrna_pvalues_high = sorted_indices
-        .iter()
-        .map(|x| sgrna_pvalues_high[*x])
-        .collect::<Array1<f64>>();
-
-    (
-        passing_gene_names,
-        passing_sgrna_pvalues_low,
-        passing_sgrna_pvalues_high,
-    )
-}
-
-/// Calculates an empirical alpha threshold for RRA
-fn calculate_empirical_alpha(pvalue_arr: &Array1<f64>, alpha: f64) -> f64 {
-    pvalue_arr
-        .map(|x| if *x < alpha { 1.0 } else { 0.0 })
-        .mean()
-        .expect("Error calculating mean in empirical alpha")
-}
-
-/// Sets the alpha threshold empirically or returns the untouched alpha
-fn set_alpha_threshold(
-    pvalue_low: &Array1<f64>,
-    pvalue_high: &Array1<f64>,
-    alpha: f64,
-    adjust_alpha: bool,
-) -> (f64, f64) {
-    if adjust_alpha {
-        (
-            calculate_empirical_alpha(pvalue_low, alpha),
-            calculate_empirical_alpha(pvalue_high, alpha),
-        )
-    } else {
-        (alpha, alpha)
+impl InternalAggregationResult {
+    pub fn new(
+        genes: Vec<String>,
+        gene_scores_low: Array1<f64>,
+        gene_pvalues_low: Array1<f64>,
+        gene_scores_high: Array1<f64>,
+        gene_pvalues_high: Array1<f64>,
+    ) -> Self {
+        Self {
+            genes,
+            gene_scores_low,
+            gene_pvalues_low,
+            gene_scores_high,
+            gene_pvalues_high,
+        }
     }
 }
+
+/// Runs the RRA gene aggregation procedure
+fn run_rra(
+    pvalue_low: &Array1<f64>,
+    pvalue_high: &Array1<f64>,
+    gene_names: &[String],
+    alpha: f64,
+    adjust_alpha: bool,
+    npermutations: usize,
+    logger: &Logger,
+) -> InternalAggregationResult {
+    let (alpha_low, alpha_high) = set_alpha_threshold(
+        &pvalue_low,
+        &pvalue_high,
+        alpha,
+        adjust_alpha,
+    );
+    logger.report_rra_alpha(alpha_low, alpha_high);
+    let (genes, gene_scores_low, gene_pvalues_low) = alpha_rra(
+        pvalue_low,
+        gene_names,
+        alpha_low,
+        npermutations,
+        logger,
+    );
+    let (_, gene_scores_high, gene_pvalues_high) = alpha_rra(
+        pvalue_high,
+        gene_names,
+        alpha_high,
+        npermutations,
+        logger,
+    );
+    InternalAggregationResult::new(genes, gene_scores_low, gene_pvalues_low, gene_scores_high, gene_pvalues_high)
+}
+
+/// Runs the INC gene aggregation procedure
+fn run_inc(
+    pvalue_low: &Array1<f64>,
+    pvalue_high: &Array1<f64>,
+    gene_names: &[String],
+    token: &str,
+    logger: &Logger,
+) -> InternalAggregationResult {
+    let (genes, gene_scores_low, gene_pvalues_low) = inc(
+        pvalue_low,
+        gene_names,
+        token,
+        logger,
+    );
+    let (_, gene_scores_high, gene_pvalues_high) = inc(
+        pvalue_high,
+        gene_names,
+        token,
+        logger,
+    );
+    InternalAggregationResult::new(genes, gene_scores_low, gene_pvalues_low, gene_scores_high, gene_pvalues_high)
+}
+
 
 /// Computes gene aggregation using the provided method and associated configurations.
 pub fn compute_aggregation(
@@ -115,128 +128,48 @@ pub fn compute_aggregation(
         logger,
     );
 
-    let (genes, gene_scores_low, gene_pvalues_low, gene_scores_high, gene_pvalues_high) = match agg
+    let agg_result = match agg
     {
         GeneAggregation::AlpaRRA {
             alpha,
             npermutations,
             adjust_alpha,
         } => {
-            let (alpha_low, alpha_high) = set_alpha_threshold(
+            run_rra(
                 &passing_sgrna_pvalues_low,
                 &passing_sgrna_pvalues_high,
+                &passing_gene_names,
                 *alpha,
                 *adjust_alpha,
-            );
-            logger.report_rra_alpha(alpha_low, alpha_high);
-
-            let (genes, gene_scores_low, gene_pvalues_low) = alpha_rra(
-                &passing_sgrna_pvalues_low,
-                &passing_gene_names,
-                alpha_low,
                 *npermutations,
                 logger,
-            );
-
-            let (_, gene_scores_high, gene_pvalues_high) = alpha_rra(
-                &passing_sgrna_pvalues_high,
-                &passing_gene_names,
-                alpha_high,
-                *npermutations,
-                logger,
-            );
-
-            (
-                genes,
-                gene_scores_low,
-                gene_pvalues_low,
-                gene_scores_high,
-                gene_pvalues_high,
             )
+
         }
         GeneAggregation::Inc { token } => {
-            let (genes, gene_scores_low, gene_pvalues_low) = inc(
+            run_inc(
                 &passing_sgrna_pvalues_low,
-                &passing_gene_names,
-                token,
-                logger,
-            );
-            let (_, gene_scores_high, gene_pvalues_high) = inc(
                 &passing_sgrna_pvalues_high,
                 &passing_gene_names,
                 token,
                 logger,
-            );
-            (
-                genes,
-                gene_scores_low,
-                gene_pvalues_low,
-                gene_scores_high,
-                gene_pvalues_high,
             )
         }
     };
 
-    let gene_fc = genes
+    let gene_fc = agg_result.genes
         .iter()
         .map(|x| gene_fc_hashmap.get(x).unwrap_or(&0.0))
         .copied()
         .collect::<Array1<f64>>();
 
     AggregationResult::new(
-        genes,
+        agg_result.genes,
         gene_fc,
-        gene_pvalues_low,
-        gene_pvalues_high,
-        gene_scores_low,
-        gene_scores_high,
+        agg_result.gene_pvalues_low,
+        agg_result.gene_pvalues_high,
+        agg_result.gene_scores_low,
+        agg_result.gene_scores_high,
         correction,
     )
-}
-
-#[cfg(test)]
-mod testing {
-    use super::{calculate_empirical_alpha, filter_zeros, mask_zeros};
-    use crate::utils::logging::Logger;
-    use ndarray::{Array1, Array2, Axis};
-    use ndarray_rand::{
-        rand_distr::{Binomial, Uniform},
-        RandomExt,
-    };
-
-    #[test]
-    fn test_empirical_alpha() {
-        let array = Array1::from_vec(vec![0.1, 0.2, 0.25, 0.5, 0.5]);
-        let alpha = 0.3;
-        let emp = calculate_empirical_alpha(&array, alpha);
-        assert_eq!(emp, 3.0 / 5.0);
-    }
-
-    #[test]
-    fn test_mask_zeros() {
-        let array = Array1::from_vec(vec![0., 1., 0., 1., 0.]);
-        let logger = Logger::new();
-        let mask = mask_zeros(&array, &logger);
-        assert_eq!(mask.len(), 2);
-        assert!(mask.contains(&1));
-        assert!(mask.contains(&3));
-    }
-
-    #[test]
-    fn test_filter_zeros() {
-        let logger = Logger::new();
-        let array = Array2::random((100, 2), Binomial::new(1, 0.2).unwrap()).mapv(|x| x as f64);
-        let means = array.mean_axis(Axis(1)).unwrap();
-        let nonzero = mask_zeros(&means, &logger);
-        let gene_names = (0..100)
-            .map(|x| format!("gene_{x}"))
-            .collect::<Vec<String>>();
-        let p_low = Array1::random(100, Uniform::new(0.0, 1.0));
-        let p_high = Array1::random(100, Uniform::new(0.0, 1.0));
-        let (pgn, ppl, pph) = filter_zeros(&array, &gene_names, &p_low, &p_high, &logger);
-
-        assert_eq!(pgn.len(), nonzero.len());
-        assert_eq!(ppl.len(), nonzero.len());
-        assert_eq!(pph.len(), nonzero.len());
-    }
 }
--- src/aggregation/utils.rs
+++ src/aggregation/utils.rs
@@ -1,8 +1,9 @@
-use hashbrown::HashMap;
-use ndarray::Array1;
+use hashbrown::{HashMap, HashSet};
+use ndarray::{Array1, Array2, Axis};
+use crate::utils::logging::Logger;
 
 /// Converts a vector of strings to an integer representation
-pub fn encode_index(genes: &Vec<String>) -> (HashMap<usize, String>, Vec<usize>) {
+pub fn encode_index(genes: &[String]) -> (HashMap<usize, String>, Vec<usize>) {
     let mut total = 0usize;
     let mut map = HashMap::with_capacity(genes.len());
     let mut encoding = Vec::with_capacity(genes.len());
@@ -32,17 +33,98 @@ pub fn select_ranks(current_idx: usize, encodings: &[usize], ranks: &Array1<f64>
         .collect()
 }
 
+/// Return all indices where values are above zero
+pub fn mask_zeros(array: &Array1<f64>, logger: &Logger) -> HashSet<usize> {
+    let mask = array
+        .iter()
+        .enumerate()
+        .filter(|(_idx, x)| **x > 0.)
+        .map(|(idx, _)| idx)
+        .collect::<HashSet<usize>>();
+    logger.num_zeros(array.len() - mask.len());
+    mask
+}
+
+/// Select from vector where indices are in the mask
+pub fn select_from_mask<T: Clone>(array: &[T], mask: &[usize]) -> Vec<T> {
+    mask.iter()
+        .map(|x| array[*x].clone())
+        .collect::<Vec<T>>()
+}
+
+/// Select from array where indices are in the mask
+pub fn select_from_mask_array<T: Clone>(array: &Array1<T>, mask: &[usize]) -> Array1<T> {
+    mask.iter()
+        .map(|x| array[*x].clone())
+        .collect::<Array1<T>>()
+}
+
+/// Filter `sgRNAs` with zero counts in both samples
+pub fn filter_zeros(
+    normed_matrix: &Array2<f64>,
+    gene_names: &[String],
+    sgrna_pvalues_low: &Array1<f64>,
+    sgrna_pvalues_high: &Array1<f64>,
+    logger: &Logger,
+) -> (Vec<String>, Array1<f64>, Array1<f64>) {
+    let sgrna_means = normed_matrix.mean_axis(Axis(1)).unwrap();
+    let passing_indices = mask_zeros(&sgrna_means, logger);
+    let mut sorted_indices = passing_indices.iter().copied().collect::<Vec<usize>>();
+    sorted_indices.sort_unstable();
+
+    let passing_gene_names = select_from_mask(gene_names, &sorted_indices);
+    let passing_sgrna_pvalues_low = select_from_mask_array(sgrna_pvalues_low, &sorted_indices);
+    let passing_sgrna_pvalues_high = select_from_mask_array(sgrna_pvalues_high, &sorted_indices);
+
+    (
+        passing_gene_names,
+        passing_sgrna_pvalues_low,
+        passing_sgrna_pvalues_high,
+    )
+}
+
+/// Calculates an empirical alpha threshold for RRA
+fn calculate_empirical_alpha(pvalue_arr: &Array1<f64>, alpha: f64) -> f64 {
+    pvalue_arr
+        .map(|x| if *x < alpha { 1.0 } else { 0.0 })
+        .mean()
+        .expect("Error calculating mean in empirical alpha")
+}
+
+/// Sets the alpha threshold empirically or returns the untouched alpha
+pub fn set_alpha_threshold(
+    pvalue_low: &Array1<f64>,
+    pvalue_high: &Array1<f64>,
+    alpha: f64,
+    adjust_alpha: bool,
+) -> (f64, f64) {
+    if adjust_alpha {
+        (
+            calculate_empirical_alpha(pvalue_low, alpha),
+            calculate_empirical_alpha(pvalue_high, alpha),
+        )
+    } else {
+        (alpha, alpha)
+    }
+}
+
+
 #[cfg(test)]
 mod testing {
-    use super::{encode_index, select_ranks};
-    use ndarray::array;
+    use super::{encode_index, select_ranks, mask_zeros, filter_zeros, calculate_empirical_alpha};
+    use ndarray::{Array1, Array2, Axis, array};
+    use ndarray_rand::{
+        rand_distr::{Binomial, Uniform},
+        RandomExt,
+    };
+    use crate::utils::logging::Logger;
 
     #[test]
     fn test_encoding() {
         let names = vec!["g.0", "g.1", "g.0", "g.2"]
             .iter()
             .map(|x| (*x).to_string())
-            .collect();
+            .collect::<Vec<String>>();
         let (encode_map, encoding) = encode_index(&names);
         assert_eq!(encoding, vec![0, 1, 0, 2]);
         assert_eq!(encode_map.get(&0), Some(&"g.0".to_string()));
@@ -58,4 +140,42 @@ mod testing {
         let selection = select_ranks(0, &indices, &scores);
         assert_eq!(selection, array![0.5, 0.5]);
     }
+
+    #[test]
+    fn test_empirical_alpha() {
+        let array = Array1::from_vec(vec![0.1, 0.2, 0.25, 0.5, 0.5]);
+        let alpha = 0.3;
+        let emp = calculate_empirical_alpha(&array, alpha);
+        assert_eq!(emp, 3.0 / 5.0);
+    }
+
+    #[test]
+    fn test_mask_zeros() {
+        let array = Array1::from_vec(vec![0., 1., 0., 1., 0.]);
+        let logger = Logger::new();
+        let mask = mask_zeros(&array, &logger);
+        assert_eq!(mask.len(), 2);
+        assert!(mask.contains(&1));
+        assert!(mask.contains(&3));
+    }
+
+    #[test]
+    fn test_filter_zeros() {
+        let logger = Logger::new();
+        let array = Array2::random((100, 2), Binomial::new(1, 0.2).unwrap()).mapv(|x| x as f64);
+        let means = array.mean_axis(Axis(1)).unwrap();
+        let nonzero = mask_zeros(&means, &logger);
+        let gene_names = (0..100)
+            .map(|x| format!("gene_{x}"))
+            .collect::<Vec<String>>();
+        let p_low = Array1::random(100, Uniform::new(0.0, 1.0));
+        let p_high = Array1::random(100, Uniform::new(0.0, 1.0));
+        let (pgn, ppl, pph) = filter_zeros(&array, &gene_names, &p_low, &p_high, &logger);
+
+        assert_eq!(pgn.len(), nonzero.len());
+        assert_eq!(ppl.len(), nonzero.len());
+        assert_eq!(pph.len(), nonzero.len());
+    }
+
+
 }
