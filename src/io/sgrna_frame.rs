ba7c1d2438b805397725ab77228fe507aa19fd5e
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-06 12:06:19 -0800

    write base mean to sgrna results output

--- src/io/sgrna_frame.rs
+++ src/io/sgrna_frame.rs
@@ -10,6 +10,7 @@ pub struct SgrnaFrame<'a> {
     sgrna_names: &'a [String],
     gene_names: &'a [String],
     adj_var: &'a Array1<f64>,
+    base: &'a Array1<f64>,
     control: &'a Array1<f64>,
     treatment: &'a Array1<f64>,
     pvalue_low: &'a Array1<f64>,
@@ -31,6 +32,7 @@ impl<'a> SgrnaFrame<'a> {
             sgrna_names,
             gene_names,
             adj_var,
+            base: sgrna_results.base_means(),
             control: sgrna_results.control_means(),
             treatment: sgrna_results.treatment_means(),
             pvalue_low: sgrna_results.pvalues_low(),
@@ -48,15 +50,16 @@ impl<'a> SgrnaFrame<'a> {
 
         writeln!(
             writer,
-            "sgrna\tgene\tcontrol\ttreatment\tadj_var\tfold_change\tlog2_fold_change\tpvalue_low\tpvalue_high\tpvalue_twosided\tfdr",
+            "sgrna\tgene\tbase\tcontrol\ttreatment\tadj_var\tfold_change\tlog2_fold_change\tpvalue_low\tpvalue_high\tpvalue_twosided\tfdr",
         )?;
 
         for idx in 0..self.size {
             writeln!(
                 writer,
-                "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{:e}\t{:e}\t{:e}\t{:e}",
+                "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{:e}\t{:e}\t{:e}\t{:e}",
                 self.sgrna_names[idx],
                 self.gene_names[idx],
+                self.base[idx],
                 self.control[idx],
                 self.treatment[idx],
                 self.adj_var[idx],
