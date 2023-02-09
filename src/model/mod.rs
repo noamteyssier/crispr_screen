1b344dfab31dfe644b403dc431699797669f81f0
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-09 11:32:46 -0800

    include math in model mod

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -5,12 +5,14 @@ mod model_mean_variance;
 mod ols;
 mod sqmean;
 mod wols;
+mod math;
 
 use logged_ols::LoggedOls;
 pub use model_mean_variance::model_mean_variance;
 use ols::Ols;
 use sqmean::Sqmean;
 use wols::Wols;
+use math::inverse;
 
 #[derive(ValueEnum, Debug, Clone)]
 pub enum ModelChoice {
